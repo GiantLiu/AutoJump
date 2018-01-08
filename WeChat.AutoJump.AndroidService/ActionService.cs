5629a5a5b54ba61b9fd717ad4b572a8fa69ab545
GiantLiu <liuju150@outlook.com>
2018-01-08 18:00:52 +0800

    修改为公用下载

--- WeChat.AutoJump.AndroidService/ActionService.cs
+++ WeChat.AutoJump.AndroidService/ActionService.cs
@@ -34,28 +34,63 @@ namespace WeChat.AutoJump.AndroidService
 
         public Bitmap GetScreenshots()
         {
+            //var adbDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AndoridAdb");
+            //var adbPath = Path.Combine(adbDirectoryPath, "adb.exe");
+            //if (!File.Exists(adbPath)) throw new NotImplementedException();
+            //var imgFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Download", DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png");
+            //using (Process process = new Process())
+            //{
+            //    //process.StartInfo.WorkingDirectory = adbDirectoryPath;
+            //    process.StartInfo.FileName = adbPath;
+            //    process.StartInfo.Arguments = "shell screencap -p | base64";
+            //    process.StartInfo.UseShellExecute = false;
+            //    process.StartInfo.RedirectStandardInput = true;   //重定向标准输入   
+            //    process.StartInfo.RedirectStandardOutput = true;  //重定向标准输出   
+            //    process.StartInfo.RedirectStandardError = true;   //重定向错误输出
+            //    process.StartInfo.CreateNoWindow = true;  
+            //    process.Start();
+            //    var memoStream = new MemoryStream();
+            //    var result= process.StandardOutput.ReadToEnd();
+            //    process.WaitForExit();
+            //    byte[] b = Convert.FromBase64String(result);
+            //    MemoryStream ms = new MemoryStream(b);
+            //    Bitmap bitmap = new Bitmap(ms);
+            //    return bitmap;
+            //}
+
             var adbDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AndoridAdb");
             var adbPath = Path.Combine(adbDirectoryPath, "adb.exe");
             if (!File.Exists(adbPath)) throw new NotImplementedException();
-            var imgFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Download", DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png");
+            var fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png";
+            var imgFileDicPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Download");
+            var createFileArg= String.Format("shell screencap -p /sdcard/{0}", fileName);
+            AdbCommand(createFileArg);
+            var downLoadArg = String.Format("pull /sdcard/{0} {1}", fileName, imgFileDicPath);
+            AdbCommand(downLoadArg);
+            var delFileArg = String.Format("shell rm /sdcard/{0}", fileName);
+            AdbCommand(delFileArg);
+            var imgFilePath = Path.Combine(imgFileDicPath, fileName);
+            if (!File.Exists(imgFilePath)) return null;
+            return new Bitmap(imgFilePath);
+        }
+        public string AdbCommand(string arg)
+        {
             using (Process process = new Process())
             {
-                //process.StartInfo.WorkingDirectory = adbDirectoryPath;
+                var adbDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AndoridAdb");
+                var adbPath = Path.Combine(adbDirectoryPath, "adb.exe");
                 process.StartInfo.FileName = adbPath;
-                process.StartInfo.Arguments = "shell screencap -p | base64";
+                process.StartInfo.Arguments = arg;
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.RedirectStandardInput = true;   //重定向标准输入   
                 process.StartInfo.RedirectStandardOutput = true;  //重定向标准输出   
                 process.StartInfo.RedirectStandardError = true;   //重定向错误输出
-                process.StartInfo.CreateNoWindow = true;  
+                process.StartInfo.CreateNoWindow = true;
                 process.Start();
-                var memoStream = new MemoryStream();
-                var result= process.StandardOutput.ReadToEnd();
+                var result = process.StandardOutput.ReadToEnd();
                 process.WaitForExit();
-                byte[] b = Convert.FromBase64String(result);
-                MemoryStream ms = new MemoryStream(b);
-                Bitmap bitmap = new Bitmap(ms);
-                return bitmap;
+                process.Close();
+                return result;
             }
         }
     }
